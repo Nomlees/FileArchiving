46ace609368f98de2417c036ed3c43bc715a5fb3
Nomlees <nomlees@bk.ru>
2018-05-03 15:18:33 +0300

    Добавлена проверка на существование файла

--- src/ru/zak/txt/Demo.java
+++ src/ru/zak/txt/Demo.java
@@ -1,5 +1,6 @@
 package ru.zak.txt;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Scanner;
@@ -8,36 +9,45 @@ import java.util.zip.ZipOutputStream;
 
 public class Demo {
     public static void main(String[] args) {
+        String filename = path();
+        File file = new File(filename);
+        if (!file.exists()) {
+            System.out.println("Проверьте путь к файлу и введите заново: ");
+            filename = path();
+        }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите путь к файлу:");
-        String filename = scanner.nextLine();
-        String[] file = filename.split("\\.");
-       // String files = file[0];
-        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(file[0]+".zip"));
+        String[] files = filename.split("\\.");
+
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(files[0]+".zip"));
              FileInputStream fis = new FileInputStream(filename)) {
-            ZipEntry entry1 = new ZipEntry("notes.txt");
+            ZipEntry entry1 = new ZipEntry("notes."+files[1]);
             zout.putNextEntry(entry1);
             // считываем содержимое файла в массив byte
             byte[] buffer = new byte[fis.available()];
 
             // добавляем содержимое к архиву
             zout.write(buffer);
-            // закрываем текущую запись для новой записи
 
+            // закрываем текущую запись для новой записи
             zout.closeEntry();
             fis.read(buffer);
-
             long size =  entry1.getCompressedSize();
             long nsize = entry1.getSize();
             System.out.println("Размер исходного файла " + nsize);
             System.out.println("Размер после сжатия " +  size);
-            System.out.println("Путь к архиву " +file[0]+".zip" );
+            System.out.println("Путь к архиву " +files[0]+".zip" );
 
         } catch (Exception ex) {
 
             System.out.println(ex.getMessage());
         }
 
+    }
+    private static String path(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к файлу:");
+        String filename = scanner.nextLine();
+
+            return filename;
     }
 }
